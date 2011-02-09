bbb571a486c99faeae19e71c9b8b607661ecbfb2
Adam Rudd <adam.rudd@uqconnect.edu.au>
2011-02-10 09:47:04 +1000

    Eww, binaries!

--- nodejs/mqttsyncserver.js
+++ nodejs/mqttsyncserver.js
@@ -8,23 +8,28 @@ var app = express.createServer();
 
 sub = spawn('mosquitto_sub', ['-v', '-t', '#']);
 
+function addMessage(topic, msg) {
+    var msgObj = new Object();
+    msgObj.timestamp = new Date();
+    msgObj.payload = msg;
+    //msgObj.topic = topic;
+
+    if(messages[topic]) {
+	messages[topic].push(msgObj);
+    } else {
+	messages[topic] = new Array(msgObj);
+    }
+}
+
 sub.stdout.on('data', function(data) {
     var split = data.toString('ascii').split(" ", 2);
 
-    var msgObj = new Object();
-    msgObj.timestamp = new Date();
-    msgObj.payload = split[1].slice(0, -1);
-
-
-    if(messages[split[0]]) {
-	messages[split[0]].push(msgObj);
-    } else {
-	messages[split[0]] = [msgObj];
-    }
+    sys.log(split);
+    addMessage(split[0], split[1].slice(0,-1));
 
     sys.log("Messages!");
     for(k in messages) {
-	sys.log("key: " + k + " value: " + messages[k]);
+	sys.log("key: " + k + " value: " + JSON.stringify(messages[k]));
     }
     sys.log("No more messages!");
 });
@@ -33,14 +38,33 @@ sub.on('exit', function(data) {
     sys.log("Shit's exitin");
 });
 
-app.get('/:topic?', function(req, res){
+app.get('/:topic', function(req, res, next){
     var topic = req.params.topic;
-    var timestamp = new Date(req.query.timestamp);
+    if(req.query.since === undefined) {
+	next();
+    }
+    var since = new Date(req.query.since);
     var json = new Object();
 
-    sys.log(topic + " " + timestamp + " requested");
+    sys.log(topic + " " + since + " requested");
 
     json.topic = topic;
+    if(messages[topic] === undefined) {
+	json.messages = [];
+    } else {
+	json.messages = messages[topic].reduce(function(v) {
+	    sys.log(JSON.stringify(v));
+	    if(v.timestamp > since) {
+		sys.log("> since");
+		return true;
+	    } else {
+		sys.log("< since");
+		return false;
+	    }
+	});
+    }
+
+    /*
     json.messages = [];
     if(messages[topic]) {
 	for(var k in messages[topic]) {
@@ -50,9 +74,27 @@ app.get('/:topic?', function(req, res){
 	    }
 	}
     }
-
+    */
+    sys.log(JSON.stringify(json));
     res.send(json);
 });
 
+app.get('/:topic', function(req, res) {
+    var topic = req.params.topic;
+    res.send({"topic":topic, "messages":messages[topic]});
+});
+
+/*
+app.post('/:topic', function(req,res) {
+    var topic = req.params.topic;
+    var msg = JSON.stringify(req.body);
+
+    sys.log(msg);
+    sys.log("Publishing message " + msg + " to topic " + topic);
+    pub = spawn('mosquitto_pub', ['-t', topic, '-m', msg]);
+
+    res.send({"success":"true"});
+});
+*/
 
 app.listen(3000);
